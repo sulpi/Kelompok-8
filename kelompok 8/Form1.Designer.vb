9eab1e4dfe8870ee5438d800b8f19ddff23f9709
Asima-PC\Asima <nrasima.wahid@gmail.com>
2016-12-08 21:10:53 -0800

    codingan form 1 ke form 2 belum bisa. tolong dibaiki

--- kelompok 8/Form1.Designer.vb
+++ kelompok 8/Form1.Designer.vb
@@ -22,6 +22,7 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
@@ -29,6 +30,9 @@ Partial Class Form1
         Me.Label3 = New System.Windows.Forms.Label()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
         Me.SuspendLayout()
         '
         'Label1
@@ -36,7 +40,7 @@ Partial Class Form1
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.ForeColor = System.Drawing.Color.Black
-        Me.Label1.Location = New System.Drawing.Point(6, 55)
+        Me.Label1.Location = New System.Drawing.Point(28, 55)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(488, 24)
         Me.Label1.TabIndex = 0
@@ -94,17 +98,41 @@ Partial Class Form1
         '
         'TextBox2
         '
-        Me.TextBox2.Location = New System.Drawing.Point(134, 140)
+        Me.TextBox2.Enabled = False
+        Me.TextBox2.Location = New System.Drawing.Point(134, 141)
         Me.TextBox2.Name = "TextBox2"
         Me.TextBox2.Size = New System.Drawing.Size(310, 20)
         Me.TextBox2.TabIndex = 6
+        Me.TextBox2.UseSystemPasswordChar = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(73, 246)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(358, 24)
+        Me.ProgressBar1.TabIndex = 7
+        '
+        'Timer1
+        '
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(453, 144)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(95, 17)
+        Me.CheckBox1.TabIndex = 8
+        Me.CheckBox1.Text = "Non Password"
+        Me.CheckBox1.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.CornflowerBlue
-        Me.ClientSize = New System.Drawing.Size(494, 253)
+        Me.ClientSize = New System.Drawing.Size(549, 282)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.ProgressBar1)
         Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Label3)
@@ -125,5 +153,8 @@ Partial Class Form1
     Friend WithEvents Label3 As System.Windows.Forms.Label
     Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
     Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
 
 End Class
--- kelompok 8/Form1.vb
+++ kelompok 8/Form1.vb
@@ -1,3 +1,39 @@
 ﻿Public Class Form1
 
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        End
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Timer1.Start()
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        If TextBox1.Text = "nama" And TextBox2.Text = "password" Then
+            ProgressBar1.Increment(10)
+            If ProgressBar1.Value = ProgressBar1.Maximum Then
+                Timer1.Stop()
+                ProgressBar1.Value = 0
+                MsgBox("Kamu Masuk", MsgBoxStyle.Information)
+                Form2.Show()
+            End If
+        Else
+            Timer1.Start()
+            ProgressBar1.Increment(10)
+            If ProgressBar1.Value = ProgressBar1.Maximum Then
+                Timer1.Stop()
+                ProgressBar1.Value = 0
+                MsgBox("gagal masuk", MsgBoxStyle.Critical)
+            End If
+        End If
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked Then
+            TextBox2.UseSystemPasswordChar = False
+        Else
+            TextBox2.UseSystemPasswordChar = True
+        End If
+    End Sub
+
 End Class
